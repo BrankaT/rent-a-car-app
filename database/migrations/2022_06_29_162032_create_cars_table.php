25421fc875036ba9e253b724314837407e068877
BrankaT <branka.topic85@gmail.com>
2022-07-02 13:11:41 +0200

    init

---
+++ database/migrations/2022_06_29_162032_create_cars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('registration_license');
+            $table->string('brand');
+            $table->string('model');
+            $table->date('manufacture_date');
+            $table->string('description')->nullable();
+            $table->string('category_id');
+            $table->string('properties')->nullable();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
